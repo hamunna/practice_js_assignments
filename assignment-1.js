887157c62cf64ca6115561ba24c5265e91c25e31
hamunna <hamunna@gmail.com>
2021-07-03 18:17:52 +0600

    Task-1 is added successfully!

---
+++ assignment-1.js
@@ -0,0 +1,12 @@
+
+// Task-1::
+//=========
+
+// On one single line, declare three variables with the following names and values:
+//---------------------------------------------------------------------------------
+
+// firstName = "JavaScript";
+// lastName = "Learner";
+// age = 25;
+
+var firsName = "JavaScript", lastNmae = "Learner", age = 25;
