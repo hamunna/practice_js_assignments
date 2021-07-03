dd50972911d69f87fbe26d6d5c35abc25a28a7cc
hamunna <hamunna@gmail.com>
2021-07-03 18:32:27 +0600

    Task-3 Added Successfully!

--- assignment-1.js
+++ assignment-1.js
@@ -29,3 +29,23 @@ let mult = x * y;
 x += y;
 
 console.log('Sum = ' + sum + ' / Subtraction = ' + sub + ' / Division = '+ divi + ' / Multiplication = '+mult + ' / Plus Assign = '+x);
+
+//----------------------------------------------------------------------------------
+
+// Task-3::
+//=========
+
+// Using variable, operation, datatype.
+//Write code & comments to describe the correct data type of the following variables:
+//-----------------------------------------------------------------------
+
+var length = 16;    // Number Data Type
+var Name = "Admin"  // String Data Type
+
+var x = {           // Object Data Type
+    firstName: "Javascript",
+    lastName: "Group"
+};
+
+
+
